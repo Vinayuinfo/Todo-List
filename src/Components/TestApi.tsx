2b62ee79c6d23e05dc84c7b37a6fc9e06b8e7161
vinayunagar1205 <vinayunagar1205@gmail.com>
2025-06-14 13:37:02 +0530

    version 9

--- src/Components/TestApi.tsx
+++ src/Components/TestApi.tsx
@@ -1,249 +1,3 @@
-// import Image from "next/image";
-// import React, { useState } from "react";
-// import "reactjs-popup/dist/index.css";
-// import Popupp from "./Popupp";
-// import { usePassword } from "@/app/Context/password";
-
-// interface User {
-//   id: number;
-//   email: string;
-//   username: string;
-//   firstName: string;
-//   lastName: string;
-//   image: string;
-//   phone: string;
-//   role: string;
-//   address: {
-//     address: string;
-//     city: string;
-//     state: string;
-//     postalCode: string;
-//   };
-// }
-
-// interface view {
-//   image: string;
-//   username: string;
-//   firstName: string;
-//   lastName: string;
-//   email: string;
-//   phone: string;
-//   userAgent: string;
-//   role: string;
-//   address: {
-//     address: string;
-//     city: string;
-//     state: string;
-//     postalCode: string;
-//   };
-// }
-
-// const TestApi = () => {
-//   const [userData, SetUserData] = useState<User[]>([]);
-//   const [view, setView] = useState<view | undefined>(undefined);
-//   const [isOpen, setIsOpen] = useState(false);
-//   const [loading, setLoading] = useState(false);
-//   const [localSetByUser, setLocalSetByUser] = useState("");
-//   const [inputValue, setInputValue] = useState("");
-
-//   const {
-//     removePassword,
-//     setPassword: setLocalStoragePassword,
-//   } = usePassword();
-
-//   function getLocalid() {
-//     setLocalStoragePassword(localSetByUser);
-//   }
-
-//   async function getFetching() {
-//     const localId = localStorage.getItem("id");
-
-//     if (localId === "1559") {
-//       setLoading(true);
-//       try {
-//         const res = await fetch("https://dummyjson.com/users");
-//         const data = await res.json();
-//         SetUserData(data.users);
-//       } catch (error) {
-//         console.error("Error fetching users:", error);
-//       } finally {
-//         console.log("Fetch attempt completed.");
-//         setLoading(false);
-//         removePassword();
-//       }
-//     } else {
-//       console.log("Invalid ID: Access denied.");
-//     }
-//   }
-
-//   function getViewprofile(id: number) {
-//     const pageview = userData.find((user) => user.id === id);
-
-//     if (pageview) {
-//       setView({
-//         image: pageview.image,
-//         username: pageview.username,
-//         firstName: pageview.firstName,
-//         lastName: pageview.lastName,
-//         email: pageview.email,
-//         phone: pageview.phone,
-//         userAgent: navigator.userAgent,
-//         role: pageview.role,
-//         address: pageview.address,
-//       });
-//     }
-//   }
-
-//   return (
-//     <div className="relative p-6 md:p-10 bg-gradient-to-br from-green-50 to-green-100 min-h-screen">
-//       <div className="sm:flex justify-between items-center">
-//         <div className="text-2xl md:text-4xl text-center font-bold text-green-800 mb-6 animate-fade-in">
-//           🌿 Login To Data Show
-//         </div>
-//         <div className="flex gap-2 justify-center items-center">
-//           <div>
-//             {localSetByUser !== "1559" ? (
-//               <p className="text-black text-center">
-//                 Please Enter Corrrect Id to Show a Button
-//               </p>
-//             ) : (
-//               ""
-//             )}
-//             <div className="flex flex-col sm:flex-row gap-2 text-black justify-center">
-//               <div>
-//                 <input
-//                   value={inputValue}
-//                   onChange={(e) => {
-//                     setInputValue(e.target.value);
-//                     setLocalSetByUser(e.target.value);
-//                   }}
-//                   className={
-//                     localSetByUser === "1559"
-//                       ? "border"
-//                       : "border-2  border-red-500"
-//                   }
-//                 />
-//                 {localSetByUser === "1559" ? (
-//                   <p>Click Login and Click Get Data</p>
-//                 ) : (
-//                   ""
-//                 )}
-//               </div>
-//               <button
-//                 type="submit"
-//                 className="bg-green-600 cursor-pointer disabled:cursor-not-allowed text-white px-6 py-2 rounded-lg hover:bg-green-700 transition-all shadow-md"
-//                 hidden={localSetByUser.trim() !== "1559"}
-//                 onClick={() => {
-//                   getLocalid();
-//                 }}
-//               >
-//                 Login
-//               </button>
-//             </div>
-//           </div>
-//           {userData && userData.length > 0 && (
-//             <div>
-//               <button
-//                 onClick={() => {
-//                   SetUserData([]);
-//                   removePassword();
-//                   setInputValue("");
-//                 }}
-//                 className="cursor-pointer bg-green-600 p-3.5 text-white rounded-lg hover:bg-green-700 transition-all shadow-md"
-//               >
-//                 Logout
-//               </button>
-//             </div>
-//           )}
-//         </div>
-//       </div>
-
-//       {/* {localSetByUser !== "1559" ? <p>Please Correct Id Enter</p> : ""} */}
-
-//       {userData && userData.length > 0 ? (
-//         <div className="overflow-x-auto rounded-xl shadow-lg bg-white">
-//           <table className="w-full border-collapse">
-//             <thead className="bg-green-600 text-white">
-//               <tr>
-//                 <th className="p-3 text-left">Avatar</th>
-//                 <th className="p-3 text-left">Role</th>
-//                 <th className="p-3 text-left">Email</th>
-//                 <th className="p-3 text-left">Phone</th>
-//                 <th className="p-3 text-left">Address</th>
-//                 <th className="p-3 text-left">Op</th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//               {userData &&
-//                 userData.length > 0 &&
-//                 userData.map(
-//                   ({ id, image, username, email, phone, address, role }) => {
-//                     return (
-//                       <tr
-//                         key={id}
-//                         className="hover:bg-green-50 transition-all border-b"
-//                       >
-//                         <td>
-//                           <Image
-//                             src={image}
-//                             height={40}
-//                             width={40}
-//                             className="rounded-full"
-//                             alt={username}
-//                           />
-//                         </td>
-//                         <td>{role}</td>
-//                         <td>{email}</td>
-//                         <td>{phone}</td>
-//                         <td>{`${address.address} ${address.address} ${address.city} ${address.state} ${address.postalCode}`}</td>
-//                         <td>
-//                           <button
-//                             className="cursor-pointer"
-//                             onClick={() => {
-//                               setIsOpen(true);
-//                               getViewprofile(id);
-//                             }}
-//                           >
-//                             View
-//                           </button>
-//                         </td>
-//                       </tr>
-//                     );
-//                   }
-//                 )}
-//             </tbody>
-//           </table>
-//           {isOpen && view && <Popupp setIsOpen={setIsOpen} view={view} />}
-//         </div>
-//       ) : (
-//         <div className="flex flex-col text-center">
-//           <p className="text-xl text-black">
-//             No data Avialble? Ok two step Follow
-//           </p>
-//           {/* <p>first is GetData</p> */}
-//           <p className="text-black">Set Your Id and Get Data</p>
-//         </div>
-//       )}
-
-//       {userData.length === 0 && localSetByUser === "1559" ? (
-//         <div className="mt-6 flex justify-center">
-//           <button
-//             onClick={getFetching}
-//             hidden={localSetByUser !== "1559"}
-//             className="bg-green-600 disabled:cursor-not-allowed cursor-pointer text-white px-6 py-2 rounded-lg hover:bg-green-700 transition-all shadow-md"
-//           >
-//             {loading === true ? "Loading.." : "GetData"}
-//           </button>
-//         </div>
-//       ) : (
-//         ""
-//       )}
-//     </div>
-//   );
-// };
-
-// export default TestApi;
-
 "use client";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
@@ -350,6 +104,12 @@ const TestApi = () => {
   const [localSetByUser, setLocalSetByUser] = useState("");
   const [inputValue, setInputValue] = useState("");
 
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
   const {
     removePassword,
     setPassword: setLocalStoragePassword,
@@ -449,15 +209,25 @@ const TestApi = () => {
           >
             Login
           </button> */}
-          <button
+          {/* <button
             type="submit"
             className="bg-green-600 text-white px-6 py-2 rounded-lg hover:bg-green-700 transition-all shadow-md disabled:cursor-not-allowed"
             hidden={localStorage.getItem("id") === "1559"}
             onClick={getLocalid}
           >
             Login
-          </button>
-          [
+          </button> */}
+          {typeof window !== "undefined" &&
+            localStorage.getItem("id") !== "1559" && (
+              <button
+                type="submit"
+                hidden={hasMounted && localStorage.getItem("id") === "1559"}
+                className="bg-green-600 text-white px-6 py-2 rounded-lg hover:bg-green-700 transition-all shadow-md disabled:cursor-not-allowed"
+                onClick={getLocalid}
+              >
+                Login
+              </button>
+            )}
           {userData.length > 0 && (
             <button
               onClick={handleLogout}
